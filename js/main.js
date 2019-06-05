b79c189756192ff0e59f408c5136d0ab127ed926
woododoCode <woododoCode@users.noreply.github.com>
2019-06-05 14:34:17 +0300

    1

--- js/main.js
+++ js/main.js
@@ -1,5 +1,21 @@
 // JavaScript Document
 $(document).ready(function(){
-	
+	$(".owl-carousel").owlCarousel({
+		margin: 10,
+		loop: true,
+		autoplay: true,
+		autoplayHoverPause: true,
+		responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:6
+        }
+    }
+	});
 	
 });
